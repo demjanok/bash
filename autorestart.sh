570156dbf52da99db2536b0f50482271a424e562
demjanok <demjanok@gmail.com>
2017-01-03 15:35:40 +0200

    Update autorestart.sh

--- autorestart.sh
+++ autorestart.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 process='php -f /var/www/eldorado/brood bot:slack'
 
-if ps ax | grep -v grep | grep $process
+if ps ax | grep -v grep | grep "$process"
 then
+    date
     echo "$process is running..."
 else
+    date
     echo "$process not running, relaunching..."
     php -f /var/www/eldorado/brood bot:slack  >> /var/log/slack.log
 fi
