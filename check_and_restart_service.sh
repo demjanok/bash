ebd5d400a5c6f1b2ce369c94ecda54d56b20dba2
demjanok <demjanok@gmail.com>
2017-07-18 11:48:18 +0300

    Update check_and_restart_service.sh

--- check_and_restart_service.sh
+++ check_and_restart_service.sh
@@ -1,14 +1,55 @@
-#!/bin/sh
-process="php -f /var/www/eldorado/brood bot:slack"
+#!/bin/bash
 
-if ps ax | grep -v grep | grep "$process"
+process='php -f /var/www/eldorado/brood bot:slack'
+
+if ps ax | grep -v grep | grep $process
 then
-    date
+	date
     echo "$process is running..."
 else
-    date
+	date
     echo "$process not running, relaunching..."
     php -f /var/www/eldorado/brood bot:slack  >> /var/log/slack.log
 fi
 
 exit 0
+
+#!/bin/bash
+
+while true
+do
+    if ps ax | grep -v grep | grep 'php -f /var/www/eldo_bike/brood queue:mail:worker'
+    then
+        echo "process is running..."
+    else
+        clear
+        date
+        echo "process not running, relaunching..."
+        php -f /var/www/eldo_bike/brood queue:mail:worker   >> /var/log/mailclub.log
+    fi
+    sleep 100
+done
+
+nohup ./myscript 0<&- &>/dev/null &
+
+###################################################################################
+
+#!/bin/bash
+
+now=$(date +%Y-%m-%d-%k-%M)
+process=$(ps aux| grep -v grep | grep "/var/www/eldo_smpp/brood queue:sms:worker" | wc -l)
+#how much processes you nead?
+treads=1
+
+if (ps aux| grep -v grep | grep "/var/www/eldo_smpp/brood queue:sms:worker" | wc -l)<1
+then 
+echo "opachki"
+else
+echo "vse ok bro"
+fi
+exit 0
+
+echo "$process not running, relaunching..."
+NOW=$(date +"%m-%d-%Y")
+FILE="backup.$NOW.tar.gz"
+echo "Backing up data to /nas42/backup.$NOW.tar.gz file, please wait..."
