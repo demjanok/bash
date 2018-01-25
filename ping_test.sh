a4d650e77b281da0a20f21bcc2eefbc71b14f0f0
demjanok <demjanok@gmail.com>
2018-01-25 16:34:52 +0200

    modified:   LICENSE 	modified:   README.md 	modified:   check_and_restart_service.sh 	modified:   empty_folder_test.sh 	modified:   first_run.sh 	modified:   ftp_transfer.sh 	modified:   ftp_transfer2.sh 	modified:   ping_test.sh 	modified:   sql to csv export 	modified:   swap_usage.sh 	modified:   webhook_to_slack

--- ping_test.sh
+++ ping_test.sh
@@ -1,9 +1,15 @@
-ping -c 3 $HOST > /dev/null 2>&1
-if [ $(echo $?) != 0 ] ; then
+HOST="1272102"
+
+ping_test () {
+  ping -c 3 $HOST > /dev/null 2>&1
+  if [ $(echo $?) != 0 ] ; then
         echo 'network error'
         date
         echo
         exit 1
         else
         echo 'ping ok'
-fi
+  fi
+}
+echo "after function"
+ping_test
